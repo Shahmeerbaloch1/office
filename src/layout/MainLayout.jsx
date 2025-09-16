d1bdc6720f94c831888af6969fed1d74a88fda35
Shahmeerbaloch1 <balochshahmeer725@gmail.com>
2025-09-16 18:16:43 +0500

    resposive side bar

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,14 +1,40 @@
+// import Sidebar from "../components/Sidebar";
+// import { Outlet } from "react-router-dom";
+
+// const MainLayout = () => {
+//   return (
+//     <div className="flex">
+//       {/* Sidebar */}
+//       <Sidebar />
+
+//       {/* Main Content */}
+//       <div className="flex-1 bg-gray-100 min-h-screen ">
+//         <Outlet />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default MainLayout;
+
+
 import Sidebar from "../components/Sidebar";
 import { Outlet } from "react-router-dom";
 
 const MainLayout = () => {
   return (
-    <div className="flex">
+    <div className="flex flex-col md:flex-row min-h-screen">
       {/* Sidebar */}
-      <Sidebar />
+      <div className="w-full md:w-64 bg-white shadow-md">
+        {/* 
+          👉 Mobile (top): full width
+          👉 Desktop: fixed width (md:w-64)
+        */}
+        <Sidebar />
+      </div>
 
       {/* Main Content */}
-      <div className="flex-1 bg-gray-100 min-h-screen ">
+      <div className="flex-1 bg-gray-100 p-4">
         <Outlet />
       </div>
     </div>
